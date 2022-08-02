8ffe52e666c49724eda29e2765d7655fea583fde
Daniel D <kibalchishthecoder@gmail.com>
2022-08-02 21:37:46 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@ use regex::Regex;
 use std::io;
 
 fn main() {
-    
+    let mut input:String = String::new();
+
+    io::stdin()
 }
+
+// Root
+//  | - String-Word-Analyzer-
+//  | 
+
+ // cd String-Word-Analyzer-
+ // code .
